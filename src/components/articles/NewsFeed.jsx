afc17bd555c16abfc5a33117bf7e37d6f53a01ea
rahulfordev <rahulforsms@gmail.com>
2023-11-19 14:38:15 +0600

    card design

--- src/components/articles/NewsFeed.jsx
+++ src/components/articles/NewsFeed.jsx
@@ -2,16 +2,16 @@
 const NewsFeed = ({ books: { title, author, publicationDate, content } }) => {
   return (
     <div className="book-container">
-      <h3>
+      <h3 className="book__title">
         <strong><em>Title:</em></strong> {title}
       </h3>
-      <h4>
+      <h4 className="book__author">
         <strong><em>Author:</em></strong> {author}
       </h4>
-      <p>
+      <p className="book__pub">
         <strong><em>Copyright:</em> </strong> {publicationDate}
       </p>
-      <p>
+      <p className="book__contetn">
         <strong><em>Content:</em></strong> {content}
       </p>
     </div>
